a3666e28128b055d1d047b5e1760c9f9087126c8
spacemanhomer <96886699+spacemanhomer@users.noreply.github.com>
2026-06-11 17:35:20 -0700

    Render world objects through camera

--- src/render.js
+++ src/render.js
@@ -2,33 +2,33 @@ import { ENEMY_GLYPH, PLAYER_GLYPH, SHOT_RANGE, SHOT_SIZE, TERRAIN_SPEEDS } from
 import { collidesWithSolidObstacle, drawObstacles } from "./obstacles.js";
 import { drawTerrain, terrainAt } from "./terrain.js";
 
-export function renderGame(ctx, state) {
-  drawTerrain(ctx, state.terrain);
-  drawObstacles(ctx, state.obstacles);
-  drawRelics(ctx, state.relics);
-  drawProjectiles(ctx, state.projectiles);
+export function renderGame(ctx, state, canvas) {
+  drawTerrain(ctx, state, canvas);
+  drawObstacles(ctx, state.obstacles, state.camera);
+  drawRelics(ctx, state.relics, state.camera);
+  drawProjectiles(ctx, state.projectiles, state.camera);
   drawAimLine(ctx, state);
-  drawPlayer(ctx, state.player);
-  drawEnemies(ctx, state.enemies);
+  drawPlayer(ctx, state.player, state.camera);
+  drawEnemies(ctx, state.enemies, state.camera);
   drawTerrainReadout(ctx, state);
 }
 
-function drawRelics(ctx, relics) {
+function drawRelics(ctx, relics, camera) {
   ctx.fillStyle = "gold";
 
   for (const relic of relics) {
-    ctx.fillRect(relic.x, relic.y, relic.size, relic.size);
+    ctx.fillRect(relic.x - camera.x, relic.y - camera.y, relic.size, relic.size);
   }
 }
 
-function drawProjectiles(ctx, projectiles) {
+function drawProjectiles(ctx, projectiles, camera) {
   ctx.fillStyle = "#f5ead2";
 
   for (const projectile of projectiles) {
     ctx.beginPath();
     ctx.arc(
-      projectile.x + projectile.size / 2,
-      projectile.y + projectile.size / 2,
+      projectile.x - camera.x + projectile.size / 2,
+      projectile.y - camera.y + projectile.size / 2,
       projectile.size / 2,
       0,
       Math.PI * 2
@@ -51,22 +51,26 @@ function drawAimLine(ctx, state) {
   ctx.strokeStyle = "rgba(245, 234, 210, 0.45)";
   ctx.lineWidth = 2;
   ctx.beginPath();
-  ctx.moveTo(origin.x, origin.y);
-  ctx.lineTo(end.x, end.y);
+  ctx.moveTo(origin.x - state.camera.x, origin.y - state.camera.y);
+  ctx.lineTo(end.x - state.camera.x, end.y - state.camera.y);
   ctx.stroke();
 }
 
-function drawPlayer(ctx, player) {
+function drawPlayer(ctx, player, camera) {
+  const screenPlayer = toScreenRect(player, camera);
+
   ctx.fillStyle = "cyan";
-  ctx.fillRect(player.x, player.y, player.size, player.size);
-  drawGlyph(ctx, PLAYER_GLYPH, player, "#f9ffff", "#06151a", 19);
+  ctx.fillRect(screenPlayer.x, screenPlayer.y, screenPlayer.size, screenPlayer.size);
+  drawGlyph(ctx, PLAYER_GLYPH, screenPlayer, "#f9ffff", "#06151a", 19);
 }
 
-function drawEnemies(ctx, enemies) {
+function drawEnemies(ctx, enemies, camera) {
   for (const enemy of enemies) {
+    const screenEnemy = toScreenRect(enemy, camera);
+
     ctx.fillStyle = "red";
-    ctx.fillRect(enemy.x, enemy.y, enemy.size, enemy.size);
-    drawGlyph(ctx, ENEMY_GLYPH, enemy, "#fff1c7", "#260000", 19);
+    ctx.fillRect(screenEnemy.x, screenEnemy.y, screenEnemy.size, screenEnemy.size);
+    drawGlyph(ctx, ENEMY_GLYPH, screenEnemy, "#fff1c7", "#260000", 19);
   }
 }
 
@@ -113,5 +117,13 @@ function centeredRect(point) {
     x: point.x - point.size / 2,
     y: point.y - point.size / 2,
     size: point.size
+  };
+}
+
+function toScreenRect(entity, camera) {
+  return {
+    ...entity,
+    x: entity.x - camera.x,
+    y: entity.y - camera.y
   };
 }
