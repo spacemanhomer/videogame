37fae3e7245b62f88a03042d17cec10f3e921573
spacemanhomer <96886699+spacemanhomer@users.noreply.github.com>
2026-06-11 18:17:43 -0700

    Tune health spawns and buckshot constants

--- src/constants.js
+++ src/constants.js
@@ -8,13 +8,16 @@ export const DESPAWN_RADIUS = 1200;
 
 export const PLAYER_START = Object.freeze({ x: 80, y: 80 });
 export const PLAYER_BASE_SPEED = 4;
+export const PLAYER_MAX_HEALTH = 9;
 export const PLAYER_GLYPH = "♘";
 export const ENEMY_GLYPH = "☥";
 export const LEVEL_UP_INTERVAL = 3;
-export const INITIAL_ENEMY_COUNT = 3;
+export const INITIAL_ENEMY_COUNT = 4;
 export const INITIAL_RELIC_COUNT = 4;
 export const MAX_RELIC_COUNT = 8;
 export const ENEMIES_PER_LEVEL = 2;
+export const HORDE_LEVEL = 5;
+export const HORDE_ENEMIES_PER_LEVEL = 4;
 
 export const ENEMY_SEEK_SPEED = 1.65;
 
@@ -23,7 +26,15 @@ export const SHOT_RANGE = 220;
 export const SHOT_SIZE = 6;
 export const SHOT_SPEED = 8;
 
+export const BUCKSHOT_COOLDOWN = 900;
+export const BUCKSHOT_PELLETS = 9;
+export const BUCKSHOT_SPREAD = 0.58;
+export const BUCKSHOT_RANGE = 170;
+export const BUCKSHOT_SIZE = 5;
+export const BUCKSHOT_SPEED = 10;
+
 export const OBSTACLE_DAMAGE_COOLDOWN = 900;
+export const SHALLOW_WATER_DAMAGE_CHANCE = 0.01;
 
 export const TERRAIN_COLORS = ["#24452b", "#3f6636", "#8b7440", "#4b3024", "#626262"];
 export const TERRAIN_SPEEDS = [1, 1.12, 0.85, 0.55, 1.3];
