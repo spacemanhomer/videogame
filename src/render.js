bc06e87f9ec015dca6ff7a46dfcf7292fba2b307
spacemanhomer <96886699+spacemanhomer@users.noreply.github.com>
2026-06-11 19:09:51 -0700

    Repair Unicode-only actor renderer

--- src/render.js
+++ src/render.js
@@ -1,11 +1,11 @@
-import { PLAYER_GLYPH, SHOT_RANGE, SHOT_SIZE } from "./constants.js";
-import { collidesWithSolidObstacle, drawObstacles } from "./obstacles.js";
+import { PLAYER_GLYPH, SHOT_RANGE } from "./constants.js";
+import { drawObstacles } from "./obstacles.js";
 import { drawTerrain, terrainMaterialAt } from "./terrain.js";
 
 export function renderGame(ctx, state, canvas) {
   drawTerrain(ctx, state, canvas);
   drawObstacles(ctx, state.obstacles, state.camera);
   drawRelics(ctx, state.relics, state.camera);
-  drawProjectiles(ctx, state.projectiles, state.camera);
-  drawAimLine(ctx, state);
-  drawPlayer(ctx, state.player
+  drawShots(ctx, state.projectiles, state.camera);
+  drawAim(ctx, state);
+  drawOne(ctx, state.player, state.camera,
