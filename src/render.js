197ef3d1c0d31854d8188f968a76a332fdda8a10
spacemanhomer <96886699+spacemanhomer@users.noreply.github.com>
2026-06-11 19:11:15 -0700

    Use Unicode actor renderer

--- src/render.js
+++ src/render.js
@@ -1,11 +1,8 @@
-import { PLAYER_GLYPH, SHOT_RANGE } from "./constants.js";
 import { drawObstacles } from "./obstacles.js";
-import { drawTerrain, terrainMaterialAt } from "./terrain.js";
-
-export function renderGame(ctx, state, canvas) {
-  drawTerrain(ctx, state, canvas);
-  drawObstacles(ctx, state.obstacles, state.camera);
-  drawRelics(ctx, state.relics, state.camera);
-  drawShots(ctx, state.projectiles, state.camera);
-  drawAim(ctx, state);
-  drawOne(ctx, state.player, state.camera,
+import { drawTerrain } from "./terrain.js";
+import { drawActors } from "./drawActors.js";
+export function renderGame(ctx,state,canvas){
+ drawTerrain(ctx,state,canvas);
+ drawObstacles(ctx,state.obstacles,state.camera);
+ drawActors(ctx,state);
+}
