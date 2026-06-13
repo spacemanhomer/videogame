4a8a47d82513b5d93b9f9546dcd10e1b2a931992
spacemanhomer <96886699+spacemanhomer@users.noreply.github.com>
2026-06-13 11:36:12 -0700

    Render animated Mandelbrot regions

--- src/render.js
+++ src/render.js
@@ -1,8 +1,11 @@
+import { drawActors } from "./drawActors.js";
+import { drawMandelbrotRegions } from "./mandelbrotRegions.js";
 import { drawObstacles } from "./obstacles.js";
 import { drawTerrain } from "./terrain.js";
-import { drawActors } from "./drawActors.js";
+
 export function renderGame(ctx,state,canvas){
  drawTerrain(ctx,state,canvas);
+ drawMandelbrotRegions(ctx,state,canvas);
  drawObstacles(ctx,state.obstacles,state.camera);
  drawActors(ctx,state);
 }
