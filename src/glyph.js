4fd032fbc331eed7531626de697e96fc472985d7
spacemanhomer <96886699+spacemanhomer@users.noreply.github.com>
2026-06-13 10:57:30 -0700

    Prefer ancient glyph font stack

--- src/glyph.js
+++ src/glyph.js
@@ -2,7 +2,7 @@ export function glyph(ctx,g,x,y,size,fill,stroke){
   ctx.save();
   ctx.textAlign="center";
   ctx.textBaseline="middle";
-  ctx.font="700 "+size+"px Georgia,serif";
+  ctx.font="700 "+size+"px 'Noto Sans Egyptian Hieroglyphs','Segoe UI Historic','Aegyptus',Georgia,serif";
   ctx.lineWidth=3;
   ctx.strokeStyle=stroke;
   ctx.fillStyle=fill;
